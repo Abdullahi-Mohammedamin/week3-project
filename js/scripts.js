f3aaf96110703ac773a16bc00e9d94f51bdc856c
Abdullahi Mohammedamin <mohammedabdillahi750@gmail.com>
2020-02-26 12:05:41 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
 $(document).ready(function() {
-$(".abuu").click(function() {
-$(".wilder").hide();
-$(".wilder").slideDown();
-$(".wilder").toggle();
+  $(".abuu").click(function() {
+    $(".abuu").toggle()
+    $(".wilder").toggle();
+  });
+  $(".wilder").click(function() {
+    $(".abuu").toggle()
+    $(".wilder").toggle();
+  });
+  $(".abo").click(function() {
+    $(".abo").toggle()
+    $(".aguero").toggle();
+  });
+  $(".aguero").click(function() {
+    $(".abo").toggle()
+    $(".aguero").toggle();
+  });
+  $(".semi").click(function() {
+    $(".semi").toggle()
+    $(".silva").toggle();
+  });
+  $(".silva").click(function() {
+    $(".semi").toggle()
+    $(".silva").toggle();
+  });
 });
-$(".abo").click(function(){
-$(".aguero").hide();
-$(".aguero").fadeIn()
-$(".aguero").toggle();
-});
-$(".semi").click(function(){
-$(".silva").hide();
-$(".silva").fadeIn()
-$(".silva").toggle();
-});
-});function submitContactForm(){
-var name = document.querySelector("#name").value;
-const data = {
-user: 'example',
-from_email: '',
-subject: '',
+function submitContactForm(){
+  var name = document.querySelector("#name").value;
+  const data = {
+    user: 'example',
+    from_email: '',
+    subject: '',
 
-};
-alert("Thank you " + name + " for contacting us. We have recieved your message");
+  };
+  alert("Thank you " + name + " for contacting us. We have recieved your message");
 }
